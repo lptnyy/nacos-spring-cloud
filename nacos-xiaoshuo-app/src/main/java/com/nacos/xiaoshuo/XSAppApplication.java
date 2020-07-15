a7c476ce827894f44071d26638799485eef9ef40
lptnii@qq.com <wangyang19>
2020-07-15 23:06:11 +0800

    增加小说服务

---
+++ nacos-xiaoshuo-app/src/main/java/com/nacos/xiaoshuo/XSAppApplication.java
@@ -0,0 +1,24 @@
+package com.nacos.xiaoshuo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableSwagger2
+@EnableFeignClients({
+        "com.nacos.system"
+})
+@ComponentScans({
+    @ComponentScan("com.nacos.system"),
+    @ComponentScan("com.nacos.common.exception"), // 载入全局异常管理
+    @ComponentScan("com.nacos.common.feign"), // 解决 服务调用之间传递 oauth2 头信息
+})
+public class XSAppApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(XSAppApplication.class, args);
+  }
+}
