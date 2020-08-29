038579289e9379871d4ef5f99ce8a8ec7fc95b14
lptnii@qq.com <wangyang19>
2020-08-29 14:36:04 +0800

    增加 视频流服务

--- nacos-oauth-server/src/main/java/com/nacos/oauth/configuration/serializer/OAuth2ExceptionJacksonSerializer.java
+++ nacos-oauth-server/src/main/java/com/nacos/oauth/configuration/serializer/OAuth2ExceptionJacksonSerializer.java
@@ -1,10 +1,14 @@
 package com.nacos.oauth.configuration.serializer;
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.nacos.common.util.ServiceResponse;
 import com.nacos.oauth.configuration.exception.ProOAuth2Exception;
+import org.springframework.web.util.HtmlUtils;
+
 import java.io.IOException;
+import java.util.Map;
 
 public class OAuth2ExceptionJacksonSerializer extends StdSerializer<ProOAuth2Exception> {
 
@@ -15,20 +19,19 @@ public class OAuth2ExceptionJacksonSerializer extends StdSerializer<ProOAuth2Exc
     @Override
     public void serialize(ProOAuth2Exception value, JsonGenerator jgen, SerializerProvider serializerProvider) throws IOException {
         jgen.writeStartObject();
-//        jgen.writeObjectField("status", value.getHttpErrorCode());
-//        String errorMessage = value.getOAuth2ErrorCode();
-//        if (errorMessage != null) {
-//            errorMessage = HtmlUtils.htmlEscape(errorMessage);
-//        }
-//        jgen.writeStringField("msg", errorMessage);
-//        if (value.getAdditionalInformation()!=null) {
-//            for (Map.Entry<String, String> entry : value.getAdditionalInformation().entrySet()) {
-//                String key = entry.getKey();
-//                String add = entry.getValue();
-//                jgen.writeStringField(key, add);
-//            }
-//        }
-        jgen.writeObject(ServiceResponse.getAuthFAIL());
+        jgen.writeObjectField("status", value.getHttpErrorCode());
+        String errorMessage = value.getOAuth2ErrorCode();
+        if (errorMessage != null) {
+            errorMessage = HtmlUtils.htmlEscape(errorMessage);
+        }
+        jgen.writeStringField("msg", errorMessage);
+        if (value.getAdditionalInformation()!=null) {
+            for (Map.Entry<String, String> entry : value.getAdditionalInformation().entrySet()) {
+                String key = entry.getKey();
+                String add = entry.getValue();
+                jgen.writeStringField(key, add);
+            }
+        }
         jgen.writeEndObject();
     }
 }
