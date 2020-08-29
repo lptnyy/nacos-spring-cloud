0f5fb7da242674f2beb21ed47541cb7d33f572d2
lptnii@qq.com <wangyang19>
2020-08-29 21:53:31 +0800

    增加ffmpeg服务

---
+++ nacos-ffmpeg-server/src/main/java/com/nacos/ffmpeg/FFmpegApplication.java
@@ -0,0 +1,18 @@
+package com.nacos.ffmpeg;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication()
+@EnableSwagger2
+@ComponentScans({
+        @ComponentScan("com.nacos.common.exception"), // 载入全局异常管理
+        @ComponentScan("com.nacos.common.feign"), // 解决 服务调用之间传递 oauth2 头信息
+})
+public class FFmpegApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FFmpegApplication.class, args);
+    }
+}
