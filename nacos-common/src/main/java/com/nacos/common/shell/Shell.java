ba3ff1ae4158da3b98935b5447b095286fa61eb9
lptnii@qq.com <wangyang19>
2020-08-29 14:43:26 +0800

    小说增加验证条件

---
+++ nacos-common/src/main/java/com/nacos/common/shell/Shell.java
@@ -0,0 +1,16 @@
+package com.nacos.common.shell;
+
+/**
+ * 执行shell 命令
+ */
+public class Shell {
+
+    /**
+     * 执行shell方法
+     * @param shell
+     * @return
+     */
+    public static String run(String shell) {
+        return "";
+    }
+}
