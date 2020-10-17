ab56bb6b387523fbc2a7da3e3cb314bf0a769aea
lptnii@qq.com <lptnii@qq.com>
2020-10-17 17:42:14 +0800

    新数据库脚本

--- sql/pro_member.sql
+++ sql/pro_member.sql
@@ -1,27 +1,41 @@
-/*
- Navicat Premium Data Transfer
+-- --------------------------------------------------------
+-- 主机:                           192.168.1.144
+-- 服务器版本:                        5.7.27-log - MySQL Community Server (GPL)
+-- 服务器操作系统:                      Linux
+-- HeidiSQL 版本:                  9.4.0.5125
+-- --------------------------------------------------------
 
- Source Server         : 本机
- Source Server Type    : MySQL
- Source Server Version : 50728
- Source Host           : localhost:3306
- Source Schema         : pro_member
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
- Target Server Type    : MySQL
- Target Server Version : 50728
- File Encoding         : 65001
 
- Date: 12/10/2020 22:27:39
-*/
+-- 导出 pro_member 的数据库结构
+CREATE DATABASE IF NOT EXISTS `pro_member` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `pro_member`;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+-- 导出  表 pro_member.pro_level 结构
+CREATE TABLE IF NOT EXISTS `pro_level` (
+  `level_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '等级id',
+  `level_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '等级名称',
+  `image` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '展示图片',
+  `integral` int(11) NOT NULL DEFAULT '0' COMMENT '等级积分',
+  `price` decimal(32,2) NOT NULL DEFAULT '0.00' COMMENT '等级价格',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`level_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='会员等级 会员等级';
 
--- ----------------------------
--- Table structure for pro_member
--- ----------------------------
-DROP TABLE IF EXISTS `pro_member`;
-CREATE TABLE `pro_member` (
+-- 正在导出表  pro_member.pro_level 的数据：~1 rows (大约)
+DELETE FROM `pro_level`;
+/*!40000 ALTER TABLE `pro_level` DISABLE KEYS */;
+INSERT INTO `pro_level` (`level_id`, `level_name`, `image`, `integral`, `price`, `create_time`) VALUES
+	(2, '普通会员', 'jpg/2020101716154170110.jpg', 0, 0.00, '2020-10-17 16:17:49');
+/*!40000 ALTER TABLE `pro_level` ENABLE KEYS */;
+
+-- 导出  表 pro_member.pro_member 结构
+CREATE TABLE IF NOT EXISTS `pro_member` (
   `member_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '会员id',
   `member_no` varchar(32) NOT NULL COMMENT '会员号',
   `nick_name` varchar(128) NOT NULL COMMENT '会员昵称',
@@ -42,20 +56,48 @@ CREATE TABLE `pro_member` (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`member_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='会员表 会员';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COMMENT='普通会员';
 
--- ----------------------------
--- Records of pro_member
--- ----------------------------
-BEGIN;
-INSERT INTO `pro_member` VALUES (4, '123', '123', '123', '123', 'jpg/2020100712133729801.jpg', '123', 'girl', '123', '123', '123', '123', '123', 123, 'normal', '123', '123', '2020-10-10 23:26:23', '2020-10-10 23:26:38');
-COMMIT;
+-- 正在导出表  pro_member.pro_member 的数据：~1 rows (大约)
+DELETE FROM `pro_member`;
+/*!40000 ALTER TABLE `pro_member` DISABLE KEYS */;
+INSERT INTO `pro_member` (`member_id`, `member_no`, `nick_name`, `user_name`, `pass_word`, `head_portrait`, `ntroduction`, `sex`, `age`, `province`, `city`, `area`, `school`, `level_Id`, `state`, `pay_password`, `withdrawal_password`, `create_time`, `update_time`) VALUES
+	(5, '123', '123', '123', '123', 'jpg/2020101716154170110.jpg', '123', 'girl', '12', '120000', '120100', '120104', '123123', 2, 'normal', '123123', '123', '2020-10-17 16:43:35', '2020-10-17 17:26:03');
+/*!40000 ALTER TABLE `pro_member` ENABLE KEYS */;
 
--- ----------------------------
--- Table structure for undo_log
--- ----------------------------
-DROP TABLE IF EXISTS `undo_log`;
-CREATE TABLE `undo_log` (
+-- 导出  表 pro_member.pro_merchant 结构
+CREATE TABLE IF NOT EXISTS `pro_merchant` (
+  `merchant_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '标示列',
+  `abbreviation` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '企业简称',
+  `user_name` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '账号',
+  `pass_word` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '密码',
+  `logo_url` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT 'logo图片',
+  `name` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '企业全名',
+  `qualification` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '商户资质材料',
+  `margin` decimal(32,2) NOT NULL DEFAULT '0.00' COMMENT '保证金额',
+  `collect_money` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '收款账号',
+  `tel` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '固定电话',
+  `phone` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '手机号码',
+  `email` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '邮箱',
+  `province` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '省份',
+  `city` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '城市',
+  `area` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '地区',
+  `address` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '详细地址',
+  `qq` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT 'qq号码',
+  `home_url` varchar(128) COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '企业官网',
+  `stat` int(11) NOT NULL DEFAULT '0' COMMENT '状态(枚举表 enterprise_stat)',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`merchant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='商户表 ';
+
+-- 正在导出表  pro_member.pro_merchant 的数据：~0 rows (大约)
+DELETE FROM `pro_merchant`;
+/*!40000 ALTER TABLE `pro_merchant` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pro_merchant` ENABLE KEYS */;
+
+-- 导出  表 pro_member.undo_log 结构
+CREATE TABLE IF NOT EXISTS `undo_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `branch_id` bigint(20) NOT NULL,
   `xid` varchar(100) NOT NULL,
@@ -67,6 +109,13 @@ CREATE TABLE `undo_log` (
   `ext` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- 正在导出表  pro_member.undo_log 的数据：~0 rows (大约)
+DELETE FROM `undo_log`;
+/*!40000 ALTER TABLE `undo_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `undo_log` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
