791722840724b69b9c9d261796a5b9f8fa641a04
root <lptnii@qq.com>
2020-10-18 22:22:00 +0800

    增加产品服务

---
+++ nacos-product-server/src/main/java/com/nacos/product/ProductApplication.java
@@ -0,0 +1,21 @@
+package com.nacos.product;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.nacos.product.mapper")
+@EnableSwagger2
+@ComponentScans({
+    @ComponentScan("com.nacos.common.exception"), // 载入全局异常管理
+    @ComponentScan("com.nacos.common.feign"), // 解决 服务调用之间传递 oauth2 头信息
+})
+public class ProductApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ProductApplication.class, args);
+  }
+}
