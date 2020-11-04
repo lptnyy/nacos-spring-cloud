92be2bcca5cde4155b8323a7c9a1d034d2cc3af6
lptnii@qq.com <lptnii@qq.com>
2020-11-04 13:28:41 +0800

    增加并行调用

--- nacos-common/src/main/java/com/nacos/common/util/ServicesThreadLocal.java
+++ nacos-common/src/main/java/com/nacos/common/util/ServicesThreadLocal.java
@@ -2,41 +2,36 @@ package com.nacos.common.util;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-public class ServicesCountDown<T> {
+public class ServicesThreadLocal<T> {
   Map<String,T> reusltMps = new HashMap<>();
   HashMap<String,ServiceThread<T>> runs = new HashMap<>();
   static final int nThreads = Runtime.getRuntime().availableProcessors();
 
   // 开启线程池
   ExecutorService pool = null;
-  CountDownLatch count = null;
 
   /**
    * 初始化现成
-   * @param size
    * @param threadSize
    * @return
    */
-  public ServicesCountDown<T> init(int size, int threadSize){
-    count = new CountDownLatch(size);
+  public ServicesThreadLocal<T> init(int threadSize){
     pool = Executors.newFixedThreadPool(threadSize);
     return this;
   }
 
-  public ServicesCountDown<T> runs() throws InterruptedException {
+  public ServicesThreadLocal<T> runs() throws InterruptedException {
     if (pool == null) {
       pool = Executors.newFixedThreadPool(nThreads);
-      count = new CountDownLatch(runs.size());
     }
+
     ConcurrentHashMap<String, Future> futures = new ConcurrentHashMap<>();
     runs.forEach((k,run)->{
       futures.put(k,pool.submit(run));
@@ -50,7 +45,6 @@ public class ServicesCountDown<T> {
         e.printStackTrace();
       }
     });
-    count.countDown();
     pool.shutdown();
     return this;
   }
@@ -64,7 +58,7 @@ public class ServicesCountDown<T> {
     }
   }
 
-  public ServicesCountDown<T> addRun(String asName, ThreadRun<T> threadRun) {
+  public ServicesThreadLocal<T> addRun(String asName, ThreadRun<T> threadRun) {
     runs.put(asName,new ServiceThread<T>().setRun(threadRun, getHttpServletRequest()));
     return this;
   }
@@ -73,7 +67,7 @@ public class ServicesCountDown<T> {
    * 验证返回数据 如果有null值 直接抛出异常
    * @return
    */
-  public ServicesCountDown<T> checkValues() throws Exception {
+  public ServicesThreadLocal<T> checkValues() throws Exception {
     for(Object result: reusltMps.values()) {
       if (result == null) {
         throw new Exception("返回的数据有null值");
